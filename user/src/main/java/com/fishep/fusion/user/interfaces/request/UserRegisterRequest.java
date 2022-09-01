5a115c0346b023ad38546436f4091c1ef73f22dd
fly.fei <fly.fei@feisu.com>
2022-09-01 17:47:23 +0800

    auth register test

--- user/src/main/java/com/fishep/fusion/user/interfaces/request/UserRegisterRequest.java
+++ user/src/main/java/com/fishep/fusion/user/interfaces/request/UserRegisterRequest.java
@@ -1,10 +1,12 @@
 package com.fishep.fusion.user.interfaces.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserRegisterRequest {
 
     public String name;
--- user/src/test/java/com/fishep/fusion/user/application/service/AuthServiceTest.java
+++ user/src/test/java/com/fishep/fusion/user/application/service/AuthServiceTest.java
@@ -1,12 +1,35 @@
 package com.fishep.fusion.user.application.service;
 
+import com.fishep.fusion.user.application.dto.UserDTO;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AuthServiceTest {
 
+    @Autowired
+    AuthService authService;
+
     @Test
+    @Order(1)
     void register() {
+        String name = "name";
+        String email = "name@email.com";
+        String password = "secretpassword";
+
+        UserDTO userDTO = authService.register(name, email, password);
+
+        assertNotNull(userDTO);
+        assertEquals(name, userDTO.getName());
+        assertEquals(email, userDTO.getEmail());
     }
 }
