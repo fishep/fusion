f166cf6c168d0ad33348ddf3bfd48b4de50283f1
fly.fei <fly.fei@feisu.com>
2022-09-13 12:08:06 +0800

    hash encode service

--- user/src/main/java/com/fishep/fusion/user/infra/extend/impl/HashServiceImpl.java
+++ user/src/main/java/com/fishep/fusion/user/infra/extend/impl/HashServiceImpl.java
@@ -1,5 +1,7 @@
 package com.fishep.fusion.user.infra.extend.impl;
 
+import cn.hutool.crypto.digest.HMac;
+import cn.hutool.crypto.digest.HmacAlgorithm;
 import com.fishep.fusion.user.domain.extend.HashService;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 public class HashServiceImpl implements HashService {
     @Override
     public String hash(String data) {
-        return "********";
+        HMac mac = new HMac(HmacAlgorithm.HmacMD5, data.getBytes());
+
+        return mac.digestHex(data);
     }
 }
