e5bdb86e70ea5baf37d73a5b5be63a7792fd410e
fly.fei <fishep@qq.com>
2024-12-04 12:09:59 +0800

    debug spring

---
+++ debug/spring/src/main/java/com/fishep/fusion/debug/spring/FusionApplication.java
@@ -0,0 +1,18 @@
+package com.fishep.fusion.debug.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author fly.fei
+ * @Date 2024/12/4 11:56
+ * @Desc
+ **/
+@SpringBootApplication
+public class FusionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FusionApplication.class, args);
+    }
+
+}
---
+++ debug/spring/src/test/java/com/fishep/fusion/debug/spring/FusionApplicationTests.java
@@ -0,0 +1,20 @@
+package com.fishep.fusion.debug.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Author fly.fei
+ * @Date 2024/12/4 12:00
+ * @Desc
+ **/
+@SpringBootTest
+class FusionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
